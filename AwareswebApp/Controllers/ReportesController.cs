2a78b1eb6ec6868f97a6d5e6ce4c53e3c2f94a73
Amaury R. Peralta Febles <amauryperalta28@gmail.com>
2014-11-21 20:47:37 -0400

    Revert "Cambios ReportesController"
    This reverts commit 1b12a13be10fd8b036c2a14d9902a47f861c90e7.

--- AwareswebApp/Controllers/ReportesController.cs
+++ AwareswebApp/Controllers/ReportesController.cs
@@ -257,23 +257,22 @@ namespace AwareswebApp.Controllers
          * @return Lista de reportes no resueltos
          */
         [Route("Reportes/getReportsUser/{username}/{contrasena}")]
-        public JsonResult getReportsUser(string userName, string contrasena)
+        public JsonResult getReportsUser(string userName1, string contrasena)
         {
             
             //Verifico si el usuario y contrasena son validos
             int usuario = (from a in db.Colaboradores
                           where a.Password == contrasena &&
-                                a.nombreUsuario == userName
+                                a.nombreUsuario == userName1
                           select a).ToList().Count;
             //Verifico si el usuario y contrasena son validos
             if (usuario == 1)
             {
                 var rep = from a in db.Reportes
-                          where a.userName == userName
+                          where a.userName == userName1
                           select new { userName = a.userName,
                                        situacion = a.situacion,
                                        sector = a.sector,
-                                       calle = a.calle,
                                        estatus = a.estatus,
                                        fechaCreacion = a.fechaCreacion.Day.ToString() + "/" + a.fechaCreacion.Month.ToString() + "/" + a.fechaCreacion.Year.ToString(),
 
